84b9fa858f978a994628b1f6487c3868f1a4e89a
wande <wander.campelo@al.infnet.edu.br>
2026-03-30 13:53:19 -0300

    chore: adiciona código legado base do kata BuildPipeline

---
+++ java/src/test/java/org/sammancoaching/PipelineTest.java
@@ -0,0 +1,17 @@
+package org.sammancoaching;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class PipelineTest {
+
+    @Test
+    void todo() {
+        // TODO: write these tests
+    }
+
+}
